f71829a0de699d2350196f41340f2403379d8df1
XiDream <verycool3580@gmail.com>
2017-12-25 21:44:40 +0800

    add MVC classes and View classes

---
+++ src/TetrisView.java
@@ -0,0 +1,5 @@
+import View.*;
+
+public class TetrisView {
+	private TetrisWindow tetrisWindow;
+}
---
+++ src/View/TetrisWindow.java
@@ -0,0 +1,9 @@
+package View;
+
+import javax.swing.JFrame;
+
+public class TetrisWindow extends JFrame {
+	
+	public TetrisWindow() {}
+	
+}
