bed51b62b2514445683a9a1baa2071a8fdf371bb
Yuukuni <a29104705@gmail.com>
2018-01-12 15:07:10 +0800

    add startLevel、dropDown()、some bug

--- src/Model/TetrisModel.java
+++ src/Model/TetrisModel.java
@@ -1,15 +1,66 @@
 package Model;
 
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
 public class TetrisModel {
 	
 	private TetrisGame game;
 	
-	public void GameStart() {
-		
-		game = new TetrisGame();
+	private boolean quit;
+	private boolean pause;
 	
+	private int[] keyCode;
+	
+	public TetrisModel() {
+		
+		keyCode = new int[] {
+			KeyEvent.VK_UP, 
+			KeyEvent.VK_LEFT, 
+			KeyEvent.VK_RIGHT, 
+			KeyEvent.VK_DOWN, 
+			KeyEvent.VK_SPACE, 
+			KeyEvent.VK_ESCAPE
+		};
+		
+	}
+	
+	public void gameStart(int startLevel) {
+
+		quit = false;
+		pause = false;
+		game = new TetrisGame(startLevel);
+	
+	}
+	
+	public boolean quit() {
+		
+		return quit;
+		
+	}
+	
+	public void quitGame() {
+		
+		quit = true;
+		
+	}
+
+	public boolean pause() {
+		
+		return pause;
+		
+	}
+	
+	public void setPause(boolean b) {
+		
+		pause = b;
+		
+	}
+	
+	public boolean gameover() {
+		
+		return game.gameover();
+		
 	}
 	
 	public int getLevel() {
@@ -24,9 +75,27 @@ public class TetrisModel {
 		
 	}
 	
-	public long getScore() {
+	public int getScore() {
 		
 		return game.getScore();
+		
+	}
+	
+	public void addScore(int i) {
+		
+		game.addScore(i);
+		
+	}
+	
+	public int getLine() {
+		
+		return game.getLine();
+		
+	}
+	
+	public int getStartLevel() {
+		
+		return game.getStartLevel();
 		
 	}
 
@@ -69,6 +138,36 @@ public class TetrisModel {
 	public void drop() {
 		
 		game.drop();
+		
+	}
+	
+	public void dropDown() {
+		
+		game.dropDown();
+		
+	}
+	
+	public int[] getKeyCode() {
+		
+		return keyCode;
+		
+	}
+	
+	public int getKeyCode(int i) {
+		
+		return keyCode[i];
+		
+	}
+	
+	public void setKeyCode(int[] keyCode) {
+		
+		this.keyCode = keyCode;
+		
+	}
+	
+	public void setKeyCode(int i, int keyCode) {
+		
+		this.keyCode[i] = keyCode;
 		
 	}
 	
